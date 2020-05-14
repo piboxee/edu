4fa10ae0e5bf2589c5404e581a2e4269e681190d
Valentin Piho <pibox.ee@gmail.com>
2020-05-14 19:58:44 +0300

    Couses list and course details for each student

--- edu/settings.py
+++ edu/settings.py
@@ -1,4 +1,5 @@
 import os
+from django.urls import reverse_lazy
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -92,7 +93,7 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
-
+LOGIN_REDIRECT_URL = reverse_lazy('student_course_list')
 
 # Internationalization
 # https://docs.djangoproject.com/en/3.0/topics/i18n/
